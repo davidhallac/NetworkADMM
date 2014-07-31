7a86467dcbf78b42d7a8e8aac3c33cd7b860530d
David Hallac <david.hallac@gmail.com>
2014-07-31 10:51:23 -0700

    Cleaned up files

--- BasicExamples/svm_distributed.py
+++ BasicExamples/svm_distributed.py
@@ -100,7 +100,7 @@ def runADMM(m, edges, inputs, lamb, rho, numiters, x, u, z, S, ids, numtests, x_
 	eabs = math.pow(10,-3) #CHANGE THESE TWO AS PARAMS
 	erel = math.pow(10,-4)
 
-	maxProcesses =  800
+	maxProcesses =  320
 	pool = Pool(processes = min(max(m, edges), maxProcesses))
 	while(iters < numiters and (r > epri or s > edual or iters < 1)):
 		#x update
