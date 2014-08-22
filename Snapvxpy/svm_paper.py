6149932cf47b0968a0923234bad714e9b86adc72
David Hallac <david.hallac@gmail.com>
2014-08-22 13:37:00 -0700

    added plotting

--- Snapvxpy/svm_paper.py
+++ Snapvxpy/svm_paper.py
@@ -56,8 +56,8 @@ def solveZ(data):
 	inputs = int(data[data.size-1])
 	lamb = data[data.size-2]
 	rho = data[data.size-3]
-	weight = data[data.size-4]
-	useConvex = data[data.size-5]
+	useConvex = data[data.size-4]
+	weight = data[data.size-5]
 	x1 = data[0:inputs]
 	x2 = data[inputs:2*inputs]
 	u1 = data[2*inputs:3*inputs]
@@ -69,32 +69,32 @@ def solveZ(data):
 		theta = max(1 - lamb*weight/(rho*LA.norm(a - b)+0.000001), 0.5) #So no divide by zero error
 		z1 = theta*a + (1-theta)*b
 		z2 = theta*b + (1-theta)*a
+		
 
 	else: #Non-convex version
 		epsilon = 0.1
 		d = LA.norm(a-b)
 		c = lamb*weight
 
-		theta1 = (-rho*(d+epsilon) + math.sqrt(math.pow(rho,2)*math.pow(d+e,2) - 8*rho*c)) / (4*rho*d)
-		phi = math.log(1 + d(1-2*theta1)/epsilon)
+		theta1 = (-rho*(d+epsilon) + math.sqrt(math.pow(rho,2)*math.pow(d+epsilon,2) - 8*rho*c)) / (4*rho*d)
+		phi = math.log(1 + d*(1-2*theta1)/epsilon)
 		objective1 = c*phi + rho*math.pow(d,2)*(math.pow(theta1,2))
 
-		theta2 = (-rho*(d+epsilon) - math.sqrt(math.pow(rho,2)*math.pow(d+e,2) - 8*rho*c)) / (4*rho*d)
-		phi = math.log(1 + d(1-2*theta2)/epsilon)
+		theta2 = (-rho*(d+epsilon) - math.sqrt(math.pow(rho,2)*math.pow(d+epsilon,2) - 8*rho*c)) / (4*rho*d)
+		phi = math.log(1 + d*(1-2*theta2)/epsilon)
 		objective2 = c*phi + rho*math.pow(d,2)*(math.pow(theta2,2))
 
 		objective3 = rho/4*math.pow(LA.norm(a-b),2)
 
 		if(min(objective1, objective2, objective3) == objective1):
 			theta = theta1
-		elif(min(objective1, objective2, objective3) == objective1):
+		elif(min(objective1, objective2, objective3) == objective2):
 			theta = theta2
 		else:
 			theta = 0.5
 
-
-		z1 = theta*a + (1-theta*b)
-		z2 = theta*a + (1-theta*b)
+		z1 = (1-theta)*a + theta*b
+		z2 = theta*a + (1-theta)*b
 	znew = np.matrix(np.concatenate([z1, z2])).reshape(2*inputs,1)
 	return znew
 
