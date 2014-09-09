b75d3e576b0fbc77c02adcb120eddb70ae81f0ba
David Hallac <david.hallac@gmail.com>
2014-09-09 07:06:34 -0700

    Changed svm size

--- Snapvxpy/svm_paper.py
+++ Snapvxpy/svm_paper.py
@@ -241,7 +241,7 @@ def runADMM(G1, sizeOptVar, sizeData, lamb, rho, numiters, x, u, z, a, edgeWeigh
 def main():
 
 	#Set parameters
-	useConvex = 0 #1 = true, 0 = false
+	useConvex = 1 #1 = true, 0 = false
 	rho = 0.0001
 	numiters = 50
 	thresh = 10
