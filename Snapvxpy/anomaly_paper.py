62d17dfedc511dbba8759d306727a5700aa14114
David Hallac <david.hallac@gmail.com>
2014-09-23 19:47:44 -0700

    Changed event detection

--- Snapvxpy/anomaly_paper.py
+++ Snapvxpy/anomaly_paper.py
@@ -374,7 +374,7 @@ def main():
 		# 	beginning = i
 		# 	#counter = counter + 1
 		#elif (x[0,i] + x[1,i] >= 0.5 and x[0,i+1] + x[1,i+1] < 0.5):	
-		if (x[0,i] + x[1,i] - (x[0,i-1] + x[1,i-1]) > 0.5):
+		if (x[0,i] + x[1,i] - (x[0,i-4] + x[1,i-4]) > 0.5):
 			#print "Event ", counter, " starts at ", beginning, "and is length ", i - beginning
 			print "Event ", counter, " at time ", i
 			counter = counter + 1
