d766ac6c8f23d51454c6d6b593315dbb74e4c698
David Hallac <david.hallac@gmail.com>
2015-01-30 08:47:55 -0800

    RUnning timing tests

--- Snapvxpy/nondistributed_svm_comparison.py
+++ Snapvxpy/nondistributed_svm_comparison.py
@@ -291,7 +291,7 @@ def runADMM(G1, sizeOptVar, sizeData, lamb, rho, numiters, x, u, z, a, edgeWeigh
 
 def main():
 
-	nodeList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+	nodeList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
 	numattempts = nodeList.__len__()
 	times = np.zeros((numattempts,2))
 
