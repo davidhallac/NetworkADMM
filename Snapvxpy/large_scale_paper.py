bc55f503adb1432d0ed9b85ff669b218b182bc7e
David Hallac <david.hallac@gmail.com>
2015-02-13 13:14:32 -0800

    Added large-scale example for paper

--- Snapvxpy/large_scale_paper.py
+++ Snapvxpy/large_scale_paper.py
@@ -186,9 +186,9 @@ def main():
 	updateVal = 2.5 #Amount to update lambda each iteration
 
 	#Size of x, the variable we solve for
-	sizeOptVar = 1000
+	sizeOptVar = 100
 	#Amount of side information at each node
-	sizeData = 1000
+	sizeData = 100
 
 
 	#Generate graph, edge weights.
@@ -220,7 +220,7 @@ def main():
 	t = time.time()
 	while(lamb <= thresh or lamb == 0):
 		t2 = time.time()
-		#max(lamb + 0.00001, rho + lamb/50)
+		#min(lamb + 0.00001, rho + lamb/50)
 		(x, u, z, pl1, pl2) = runADMM(G1, sizeOptVar, sizeData, lamb, min(lamb + 0.00001, rho + lamb/50), numiters, x, u ,z, a, edgeWeights)
 		print "Lambda = ", lamb
 		if(lamb == 0):
